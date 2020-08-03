6ff093a54fbb1405f2081c6943177e7656750006
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-04 06:44:41 +0700

    - clean up project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App  = () => {
+	return (
+		<div className="App">
+			<h1>Lets build Covid19 Tracker Dashboard</h1>
+			{/* Header */}
+			{/* Title + Select Dropdown */}
+
+			{/* Infoboxs */}
+			{/* Infoboxs */}
+			{/* Infoboxs */}
+
+			{/* Table Cases Count */}
+			{/* Graph */}
+
+			{/* Maps */}
+		</div>
+	);
 }
 
 export default App;
