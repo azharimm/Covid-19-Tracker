c8ea586fc53ccc86e763946e62b379b2c6cd8999
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-04 07:48:35 +0700

    ^ header and countries dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FormControl, Select, MenuItem } from '@material-ui/core'
 import './App.css';
 
 const App  = () => {
+	const [countries, setCountries] = useState([]);
+
+	// https://disease.sh/v3/covid-19/countries
+	
+	useEffect(() => {
+		const getCountriesData = async () => {
+			const response = await fetch('https://disease.sh/v3/covid-19/countries')
+			const responseData = await response.json();
+			const countriesData = responseData.map(country => {
+				return {
+					name: country.country,
+					value: country.countryInfo.iso2
+				}
+			})
+
+			setCountries(countriesData)
+		}
+
+		getCountriesData();
+
+	}, []);
+
 	return (
 		<div className="App">
-			<h1>Lets build Covid19 Tracker Dashboard</h1>
+			<div className="app_header">
+				<h1>Covid-19 Tracker</h1>
+				<FormControl className="app_dropdown">
+					<Select
+					variant="outlined"
+					value="abc">
+						{
+							countries.map((country, index) => (
+								<MenuItem value={country.value} key={index}>{country.name}</MenuItem>
+							))
+						}
+					</Select>
+				</FormControl>
+			</div>
 			{/* Header */}
 			{/* Title + Select Dropdown */}
 
